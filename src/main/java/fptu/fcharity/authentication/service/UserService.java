b35fa0fbcc0de5d3fa5ff3aeb6306fc51c58b7a1
aurora-mei <huyencttde180614@fpt.edu.vn>
2025-02-06 16:38:05 +0700

    fix register + db

--- src/main/java/fptu/fcharity/authentication/service/UserService.java
+++ src/main/java/fptu/fcharity/authentication/service/UserService.java
@@ -2,6 +2,7 @@ package fptu.fcharity.authentication.service;
 
 import fptu.fcharity.authentication.entity.User;
 import fptu.fcharity.authentication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,14 +10,10 @@ import java.util.List;
 
 @Service
 public class UserService {
-    private final UserRepository userRepository;
-    public UserService(UserRepository userRepository, EmailService emailService) {
-        this.userRepository = userRepository;
-    }
+    @Autowired
+    private UserRepository userRepository;
 
     public List<User> allUsers() {
-        List<User> users = new ArrayList<>();
-        userRepository.findAll().forEach(users::add);
-        return users;
+        return userRepository.findAll();
     }
 }
