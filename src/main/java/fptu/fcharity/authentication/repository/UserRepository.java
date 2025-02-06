e0f1384df094641f2afbfd0de66a2d2c1d09b2ff
aurora-mei <huyencttde180614@fpt.edu.vn>
2025-02-06 16:40:04 +0700

    fix register + db

--- src/main/java/fptu/fcharity/authentication/repository/UserRepository.java
+++ src/main/java/fptu/fcharity/authentication/repository/UserRepository.java
@@ -2,7 +2,6 @@ package fptu.fcharity.authentication.repository;
 
 import fptu.fcharity.authentication.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
--- src/main/java/fptu/fcharity/authentication/service/UserService.java
+++ src/main/java/fptu/fcharity/authentication/service/UserService.java
@@ -5,7 +5,6 @@ import fptu.fcharity.authentication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
