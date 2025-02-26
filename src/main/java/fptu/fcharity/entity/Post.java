a0a54fe404291ea1b6514be16d72722774d407f2
gianglamnguyen <133856538+gianglamnguyen@users.noreply.github.com>
2025-02-26 16:56:12 +0700

    Post

--- src/main/java/fptu/fcharity/entity/Post.java
+++ src/main/java/fptu/fcharity/entity/Post.java
@@ -3,6 +3,9 @@ package fptu.fcharity.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -12,7 +15,12 @@ import java.util.UUID;
 @Getter
 @Setter
 public class Post {
+
     @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    // Với SQL Server, sử dụng UNIQUEIDENTIFIER (map qua kiểu VARCHAR)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
     @Column(name = "post_id", columnDefinition = "UNIQUEIDENTIFIER", updatable = false, nullable = false)
     private UUID postId;
 
@@ -23,19 +31,26 @@ public class Post {
     @Column(name = "title", nullable = false)
     private String title;
 
-    @Column(name = "content", nullable = false)
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
     private String content;
 
     @Column(name = "vote", nullable = false)
     private int vote;
 
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
-    @ManyToOne
-    @JoinColumn(name = "tag_id", nullable = false)
-    private Tag tag;
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
