d3da68a743bc1cff13d259d444f8723827d5a283
Thợ săn bé trai <kthkun@gmail.com>
2025-03-05 14:09:48 +0700

    fx

--- src/main/java/fptu/fcharity/utils/mapper/UserResponseMapper.java
+++ src/main/java/fptu/fcharity/utils/mapper/UserResponseMapper.java
@@ -4,6 +4,7 @@ import fptu.fcharity.entity.User;
 import fptu.fcharity.response.authentication.UserResponse;
 import org.mapstruct.Mapper;
 
+
 @Mapper(componentModel = "spring")
 public interface UserResponseMapper extends BaseMapper<User, UserResponse> {
 }
