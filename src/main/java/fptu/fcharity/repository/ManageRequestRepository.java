41352ad20eca3072496c78a17d7f47e78d30ab8a
Nguyên <107854408+tnguyen0307@users.noreply.github.com>
2025-03-12 16:14:06 +0700

    manage-request

---
+++ src/main/java/fptu/fcharity/repository/ManageRequestRepository.java
@@ -0,0 +1,13 @@
+package fptu.fcharity.repository;
+
+import fptu.fcharity.entity.Request;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ManageRequestRepository extends JpaRepository<Request, UUID> {
+    List<Request> findByStatus(String status);
+    Optional<Request> findByEmail(String email);
+}
