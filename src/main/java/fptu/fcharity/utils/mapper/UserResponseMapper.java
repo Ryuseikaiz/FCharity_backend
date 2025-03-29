9a2004f83da3c34433ccbf8bf2ca71f5ce91ffdf
Thợ săn bé trai <kthkun@gmail.com>
2025-03-30 03:38:18 +0700

    appplication configuration

--- src/main/java/fptu/fcharity/utils/mapper/UserResponseMapper.java
+++ src/main/java/fptu/fcharity/utils/mapper/UserResponseMapper.java
@@ -3,8 +3,13 @@ package fptu.fcharity.utils.mapper;
 import fptu.fcharity.entity.User;
 import fptu.fcharity.response.authentication.UserResponse;
 import org.mapstruct.Mapper;
-
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserResponseMapper extends BaseMapper<User, UserResponse> {
+    @Mapping(source = "id", target = "userId")
+    UserResponse toDTO(User entity);
+
+    @Mapping(source = "userId", target = "id")
+    User toEntity(UserResponse dto);
 }
