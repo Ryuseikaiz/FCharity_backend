ab701eceb62c3d052a29789d7ce7d44765875efc
ducnvidiak <ducnivival@gmail.com>
2025-04-04 15:48:02 +0700

    add apis for organizations, organizationRequests, organizationMembers

--- src/main/java/fptu/fcharity/service/manage/organization/OrganizationService.java
+++ src/main/java/fptu/fcharity/service/manage/organization/OrganizationService.java
@@ -1,21 +1,4 @@
 package fptu.fcharity.service.manage.organization;
 
-import fptu.fcharity.dto.organization.OrganizationDto;
-import fptu.fcharity.entity.Organization;
-import org.springframework.http.ResponseEntity;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.UUID;
-
 public interface OrganizationService {
-    List<OrganizationDto> findAll();
-    OrganizationDto findById(UUID id);
-    Organization findEntityById(UUID id);
-    OrganizationDto createOrganization(OrganizationDto organizationDto) throws IOException;
-    OrganizationDto updateOrganization(OrganizationDto organizationDto) throws IOException;
-    void deleteOrganization(UUID id);
-    List<OrganizationDto> getOrganizationsByCeoOrManager(UUID ceoManagerId);
-    OrganizationDto getOrganizationByOrganizationIdAndCeoOrManager(UUID id, UUID ceoManagerId);
-    OrganizationDto getMyOrganization(UUID userId);
 }
