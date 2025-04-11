de3ab8a0d114a7460a86334010527313b4a7d81f
gianglamnguyen <133856538+gianglamnguyen@users.noreply.github.com>
2025-04-11 15:30:59 +0700

    Revert "update"
    This reverts commit b83373a80d505897762b16143368372eff587ed0.

--- src/main/java/fptu/fcharity/entity/PostVoteId.java
+++ src/main/java/fptu/fcharity/entity/PostVoteId.java
@@ -6,32 +6,19 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.Hibernate;
 
-import java.io.Serial;
-import java.io.Serializable;
 import java.util.Objects;
 import java.util.UUID;
 
 @Getter
 @Setter
 @Embeddable
-public class PostVoteId implements Serializable {
-    @Serial
+public class PostVoteId implements java.io.Serializable {
     private static final long serialVersionUID = 3615165364424463624L;
-
     @Column(name = "post_id", nullable = false)
     private UUID postId;
 
     @Column(name = "user_id", nullable = false)
     private UUID userId;
-
-    // ✅ Constructor bổ sung
-    public PostVoteId(UUID postId, UUID userId) {
-        this.postId = postId;
-        this.userId = userId;
-    }
-
-    // ✅ Constructor mặc định (bắt buộc)
-    public PostVoteId() {}
 
     @Override
     public boolean equals(Object o) {
@@ -46,4 +33,5 @@ public class PostVoteId implements Serializable {
     public int hashCode() {
         return Objects.hash(postId, userId);
     }
+
 }
